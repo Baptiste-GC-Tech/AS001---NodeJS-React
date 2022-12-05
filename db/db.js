b9cab1a3cd3eb51e2673a2c9ac2345eece3ae7fa
unknown <bboismenu@gamingcampus.local>
2022-12-05 20:16:22 +0100

    init

---
+++ db/db.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+const connectionString = "mongodb+srv://baptiste:Baptiste@cluster0.hyneekk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return err;
+      }
+      
+      dbConnection = db.db("pokedex");
+      console.log("Successfully connected to MongoDB.");
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
