1512358e4091815b0f977788c1f55193a4035c00
unknown <bboismenu@gamingcampus.local>
2022-12-06 20:07:19 +0100

    Suavegarde

--- index.js
+++ index.js
@@ -19,24 +19,26 @@ app.listen(port, function () {
 })
 
 app.get("/pokemon/list", function (req, res) {
-    const db = dbo.getDb()
-    db
-      .collection("pokemon")
-      .find({})
-      .toArray(function (err, result) {
-        if (err){
-          res.status(400).send("Error fetching pokemons!")
-        }
-        res.json(result)
-      })
+  const db = dbo.getDb()
+  const coll = db.collection("pokemon")
+  const body = req.body
+
+  coll.find({}).toArray(function (err, result) {
+    if (err)
+    {
+      res.status(400).send("Error fetching pokemons!")
+    }
+    res.json(result)
+  })
 })
 
 app.post("/pokemon/insert", jsonParser, (req, res) => {
+  const db = dbo.getDb()
+  const coll = db.collection("pokemon")
   const body = req.body
   console.log('Got body:', body)
 
-  const db = dbo.getDb()
-  db.collection("pokemon").insertOne(body)
+  coll.insertOne(body)
 
   res.json(body)
 })
@@ -51,6 +53,23 @@ app.delete("/pokemon/delete", jsonParser, async (req, res) => {
   }else if(body.name){
     const query = { name:body.name }
   }
+
+  app.post('/pokemon/update', jsonParser, (req, res) => {
+    const option = { upsert: true };
+    const filter = {name:req.body.target};
+    const newBatard = {
+      $set:{
+        name: req.body.newName,
+      }
+    }
+    console.log('Got body:', newBatard);
+    
+    const db = dbo.getDb()
+    const coll = db.collection("pokemon")
+    coll.updateOne(filter,newBatard,option);
+    res.json(newBatard);
+  });
+  
 
   const result = await coll.deleteOne(query) 
   if(result.deletedCount == 1)
